c6497ccb6a3f8d04a39b6304333b33b6e8ea58ab
Zejd <zcelenka1@etf.unsa.ba>
2021-03-18 14:32:18 +0100

    Added UI for the console

--- screens/ConsoleScreen/ConsoleScreen.js
+++ screens/ConsoleScreen/ConsoleScreen.js
@@ -1,10 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+const styles = StyleSheet.create({
+  textArea: {
+    textAlignVertical: "top",
+    height: "93%",
+    justifyContent: "flex-start",
+    backgroundColor:"#012456",
+    color: "#D6FFFF"
+  },
+  inputArea:{
+    height: "7%",
+    justifyContent: "flex-start",
+    backgroundColor:"#415c77",
+    color: "#e5eaea",
+  }
+})
 
 export default function ConsoleScreen({navigation}) {
-    return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Console screen</Text>
-        </View>
-      );
+  const [history,setHistory] = useState("");
+  const [current,setCurrent] = useState("");
+  return(
+    <View>
+      <TextInput
+        style={styles.textArea}
+        underlineColorAndroid="transparent"
+        placeholder="Console >"
+        placeholderTextColor="grey"
+        multiline={true}
+        editable = {false}
+        value = {history}
+      />
+      <TextInput
+        style={styles.inputArea}
+        underlineColorAndroid="transparent"
+        placeholder="Enter your commands here"
+        placeholderTextColor="#e5eaea"
+        value={current}
+        onChangeText={(e)=>setCurrent(e)}
+        onSubmitEditing={(event) => {
+          setHistory(history+"\nconsole >"+event.nativeEvent.text);
+          setCurrent("");
+        }}
+      ></TextInput>
+    </View>
+  );
 }
