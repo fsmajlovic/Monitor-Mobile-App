736c84a339ffdd0ac678b5a275ca72797fe1e0ce
hcorbo <hcorbo2@etf.unsa.ba>
2021-03-19 21:56:09 +0100

    Dodao navigaciju i schedule screen

---
+++ screens/TehnicianScreen/screens/AddTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, View, Button, Text, TextInput } from 'react-native'; 
+
+
+export default function AddTask() {
+
+  return (
+    <View>
+    </View>
+  );
+}
