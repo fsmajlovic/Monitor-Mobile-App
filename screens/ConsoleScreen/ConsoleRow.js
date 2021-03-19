ce7bb5cab3fa1f9e1c3210832afd58a6c680acdc
lhodzic1 <lhodzic1@etf.unsa.ba>
2021-03-19 13:32:57 +0100

    Dodana validacija komandi

--- screens/ConsoleScreen/ConsoleRow.js
+++ screens/ConsoleScreen/ConsoleRow.js
@@ -4,14 +4,48 @@ import {styles} from './Styles'
   
 export default function ConsoleRow({rows}) {
 
+  const group1 = ["?", "clear", "ls"];
+  const group2 = ["cd", "echo", "erase", "kill", "move", "rd", "set"];
+
   return(
     <View>
         {rows.map((row,index) => {
-          return(
+          let args = row.split(" ");
+          let command = "";
+          command = args[0];
+
+          //komanda iz grupe 1 i nije uneseno ništa iza nje || komanda iz grupe 2 i unesen 1 parametar
+          if( (group1.includes(command) && args.length == 1) || (group2.includes(command) && args.length == 2) ) {
+            //komanda validna
+            return(
+              <View key={index}>
+                <View style={styles.row}>
+                  <Text style={styles.textArea}> IWM console > {row}</Text>
+                </View>
+                <View style={styles.row}>
+                  <Text style={styles.textArea}> Valid Command! </Text>
+              </View>
+            </View>
+            ) 
+          } else {
+            //nevalidna komanda
+            return(
+              <View key={index}>
+                <View style={styles.row}>
+                  <Text style={styles.textArea}> IWM console > {row}</Text>
+                </View>
+                <View style={styles.row}>
+                  <Text style={styles.textArea}> Invalid Command! </Text>
+              </View>
+            </View>
+            ) 
+          }
+
+        /*  return(
             <View style={styles.row} key={index}>
               <Text style={styles.textArea}> IWM console > {row}</Text>
             </View>
-          ) 
+          ) */
         })}
   </View>
 )};
