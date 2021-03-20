4dd0f6aeb884b1f45320aebf7ae77a1caa462986
hcorbo <hcorbo2@etf.unsa.ba>
2021-03-20 18:34:33 +0100

    Doradio formu i ucitavanje taskova.

--- screens/TehnicianScreen/screens/AddTask.js
+++ screens/TehnicianScreen/screens/AddTask.js
@@ -1,31 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Button, Text, TextInput } from 'react-native'; 
-
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Formik } from 'formik';
+import { Fontisto } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function AddTask() {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder={'Lokacija'}
-        style={styles.input}
-      />
+      <Formik
+        initialValues={{ location: '', description: ''}}
+        onSubmit={(values) => {
+          console.log({...values, time: Date.parse(date)});
+        }}
+      >
+        {props => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder='Location'
+              onChangeText={props.handleChange('location')}
+              value={props.values.title}
+            />
 
-      <Button
-        title={'Odaberi vrijeme'}
-        //onPress={() => this.props.navigation.navigate('datepicker.js')}
-      />
+            <TextInput
+              style={styles.input2}
+              multiline
+              placeholder='Description...'
+              onChangeText={props.handleChange('description')}
+              value={props.values.body}
+            />
 
-      <TextInput
-        placeholder={'Opis problema...'}
-        secureTextEntry={true}
-        style={styles.input2}
-      />
-
-      <Button
-        title={'Dodaj u kalendar'}
-        style={styles.input}
-      />
+            <View style={styles.conainerIcon}>
+              <Text style={styles.text}>Select date: </Text>
+              <Fontisto name="date" onPress={showDatepicker} size={24} color="black" />
+            </View>
+            <View style={styles.conainerIcon}>
+              <Text style={styles.text}>Select time: </Text>
+              <Ionicons name="ios-time-outline" onPress={showTimepicker} size={24} color="black" />
+            </View>
+                {show && (
+                  <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                  />
+                )}
+            
+            <Button title="Add in calendar" onPress={props.handleSubmit} /> 
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
@@ -34,17 +85,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  time1: {
-    width: 300,
-    height: 200,
-    backgroundColor: '#ecf0f1',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
+    justifyContent: 'flex-start',
+    marginTop: 20,
   },
   input: {
     width: 200,
@@ -53,17 +95,26 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'black',
     marginBottom: 10,
-    marginTop: 10
+    marginTop: 10,
+    fontSize: 16
   },
   input2: {
-    width: 200,
-    height: 200,
+    width: 250,
+    height: 150,
     padding: 10,
     borderWidth: 1,
     borderColor: 'black',
     marginBottom: 10,
-    marginTop: 10
+    marginTop: 10,
+    textAlignVertical: 'top',
+    fontSize: 16
   },
-
+  conainerIcon: {
+    flexDirection: "row",
+    marginVertical: 15,
+  },
+  text: {
+    fontSize: 16
+  }
 });
 
--- screens/TehnicianScreen/screens/Schedule.js
+++ screens/TehnicianScreen/screens/Schedule.js
@@ -1,61 +1,73 @@
 import React, { useState, useEffect } from 'react';
-import { Agenda} from 'react-native-calendars';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import { Text, View, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Card } from 'react-native-paper';
+import { format } from 'date-fns';
 
-const timeToString = (time) => {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
+// const timeToString = (time) => {
+//     const date = new Date(time);
+//     console.log("sati");
+//     console.log(date.getHours() + ":" + date.getMinutes());
+//     const splitDate = date.toISOString().split('T');
+//     console.log(date.toISOString());
+//     return date.toISOString().split('T')[0];
+//   }
+
+const renderItem = (item) => {
+    return <TouchableOpacity style={{marginRight: 17, marginTop: 17}}>
+        <Card>
+            <Card.Content>
+                <View style={styles.item}>
+                    <Text>{format(item.Time, 'HH:mm ')}</Text>
+                    <Text>{item.Location}</Text>
+                </View>
+                <View>
+                    <Text>{item.Description}</Text>
+                </View>
+            </Card.Content>
+        </Card>
+    </TouchableOpacity>
   }
 
-function Schedule() {
+
+function Schedule(props) {
     const [items, setItems] = useState({});
 
-    const loadItems = (day) => {
-        setTimeout(() => {
-          for (let i = -10; i < 35; i++) {
-            const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-            const strTime = timeToString(time);
-            if (!items[strTime]) {
-              items[strTime] = [];
-              const numItems = Math.floor(Math.random() * 3 + 1);
-              for (let j = 0; j < numItems; j++) {
-                items[strTime].push({
-                  name: 'Item for ' + strTime + ' #' + j,
-                  height: Math.max(50, Math.floor(Math.random() * 150))
-                });
-              }
-            }
-          }
-          const newItems = {};
-          Object.keys(items).forEach(key => {
-            newItems[key] = items[key];
-          });
-          setItems(newItems);
-        }, 1000);
-      }
 
-      const renderItem = (item) => {
-        return <TouchableOpacity style={{marginRight: 17, marginTop: 17}}>
-            <Card>
-                <Card.Content>
-                    <View style={styles.item}>
-                        <Text>Task</Text>
-                        <Text>{item.name}</Text>
-                    </View>
-                </Card.Content>
-            </Card>
-        </TouchableOpacity>
-      }
+    // useEffect(() => {
+    //     (async () => {
+    //         const response = await fetch("http://10.0.2.2:3001/tasks");
+    //         const data = await response.json();
+
+    //         const mappedData = data.map((post) => {
+    //             const date = new Date(post.Time);
+
+    //             return {
+    //                 ...post,
+    //                 date: format(date, 'yyyy-MM-dd'),
+    //             }
+    //         })
+
+    //         const reduced = mappedData.reduce((acc, currentItem) => {
+    //             const { date, ...item } = currentItem;
+    //             {acc[date] ? acc[date].push({...item}) : acc[date] = [item] }
+    //             return acc;
+    //         }, {});
+
+    //          setItems(reduced);
+    //         // console.log(timeToString(items[0].Time));
+    //     })();
+    // }, []);
+
+    
 
     return (
         <View style={{flex: 1}}>
             <Agenda
-                items={items}
-                loadItemsForMonth={loadItems}
-                selected={'2021-05-16'}
-                renderItem={renderItem}
+                // items={items}
+                // selected={'2021-3-20'}
+                // renderItem={renderItem}
             />
         </View>
     );
