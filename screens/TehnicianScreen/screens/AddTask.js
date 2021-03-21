406a56968f68d8617418562bf99b534437accb59
hcorbo <hcorbo2@etf.unsa.ba>
2021-03-21 21:43:51 +0100

    Dodali post i get zahtjeve za taskove

--- screens/TehnicianScreen/screens/AddTask.js
+++ screens/TehnicianScreen/screens/AddTask.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, TextInput } from 'react-native'; 
+import { StyleSheet, View, Button, Text, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native'; 
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { Formik } from 'formik';
 import { Fontisto } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import {AuthContext} from '../../../contexts/authContext';
+
+
+async function postScreenshot({token, location, description, date}) {
+  try {
+    let response = await fetch("https://si-2021.167.99.244.168.nip.io/api/UserTasks", {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      body: JSON.stringify({deviceId: null,
+      time: date,
+      location: location,
+      description: description})
+    });
+    var json = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
 export default function AddTask() {
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
+  var {getSavedToken} = React.useContext(AuthContext);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -30,54 +54,57 @@ export default function AddTask() {
   };
 
   return (
-    <View style={styles.container}>
-      <Formik
-        initialValues={{ location: '', description: ''}}
-        onSubmit={(values) => {
-          console.log({...values, time: Date.parse(date)});
-        }}
-      >
-        {props => (
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder='Location'
-              onChangeText={props.handleChange('location')}
-              value={props.values.title}
-            />
+    <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss(); }}>
+      <View style={styles.container}>
+        <Formik
+          initialValues={{ location: '', description: ''}}
+        >
+          {props => (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder='Location'
+                onChangeText={props.handleChange('location')}
+                value={props.values.title}
+              />
 
-            <TextInput
-              style={styles.input2}
-              multiline
-              placeholder='Description...'
-              onChangeText={props.handleChange('description')}
-              value={props.values.body}
-            />
+              <TextInput
+                style={styles.input2}
+                multiline
+                placeholder='Description...'
+                onChangeText={props.handleChange('description')}
+                value={props.values.description}
+              />
 
-            <View style={styles.conainerIcon}>
-              <Text style={styles.text}>Select date: </Text>
-              <Fontisto name="date" onPress={showDatepicker} size={24} color="black" />
+              <View style={styles.conainerIcon}>
+                <Text style={styles.text}>Select date: </Text>
+                <Fontisto name="date" onPress={showDatepicker} size={24} color="black" />
+              </View>
+              <View style={styles.conainerIcon}>
+                <Text style={styles.text}>Select time: </Text>
+                <Ionicons name="ios-time-outline" onPress={showTimepicker} size={24} color="black" />
+              </View>
+                  {show && (
+                    <DateTimePicker
+                      testID="dateTimePicker"
+                      value={date}
+                      mode={mode}
+                      is24Hour={true}
+                      display="default"
+                      onChange={onChange}
+                    />
+                  )}
+              
+              <Button title="Add in calendar" onPress={
+                async () => {
+                  let token = await getSavedToken();
+                  await postScreenshot({token, description: props.values.description, location: props.values.location, date});
+              }} />
             </View>
-            <View style={styles.conainerIcon}>
-              <Text style={styles.text}>Select time: </Text>
-              <Ionicons name="ios-time-outline" onPress={showTimepicker} size={24} color="black" />
-            </View>
-                {show && (
-                  <DateTimePicker
-                    testID="dateTimePicker"
-                    value={date}
-                    mode={mode}
-                    is24Hour={true}
-                    display="default"
-                    onChange={onChange}
-                  />
-                )}
-            
-            <Button title="Add in calendar" onPress={props.handleSubmit} /> 
-          </View>
-        )}
-      </Formik>
-    </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
--- screens/TehnicianScreen/screens/Schedule.js
+++ screens/TehnicianScreen/screens/Schedule.js
@@ -1,29 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Agenda } from 'react-native-calendars';
 import { Text, View, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Card } from 'react-native-paper';
 import { format } from 'date-fns';
+import {AuthContext} from '../../../contexts/authContext';
 
-// const timeToString = (time) => {
-//     const date = new Date(time);
-//     console.log("sati");
-//     console.log(date.getHours() + ":" + date.getMinutes());
-//     const splitDate = date.toISOString().split('T');
-//     console.log(date.toISOString());
-//     return date.toISOString().split('T')[0];
-//   }
 
 const renderItem = (item) => {
     return <TouchableOpacity style={{marginRight: 17, marginTop: 17}}>
         <Card>
             <Card.Content>
                 <View style={styles.item}>
-                    <Text>{format(item.Time, 'HH:mm ')}</Text>
-                    <Text>{item.Location}</Text>
+                    <Text>{format(Date.parse(item.time), 'HH:mm ')}</Text>
+                    <Text>{item.location}</Text>
                 </View>
                 <View>
-                    <Text>{item.Description}</Text>
+                    <Text>{item.description}</Text>
                 </View>
             </Card.Content>
         </Card>
@@ -33,41 +26,46 @@ const renderItem = (item) => {
 
 function Schedule(props) {
     const [items, setItems] = useState({});
+    const {getSavedToken} = React.useContext(AuthContext);
+
+      useEffect(()=>{
+        async function getData(getSavedToken){
+            try {
+                const token = await getSavedToken();
+                const response = await fetch("https://si-2021.167.99.244.168.nip.io/api/UserTasks", {
+                    method: 'GET',
+                    headers: {"Authorization" : "Bearer "+ token},
+                  });
+                  var data = await response.json();
+
+                const mappedData = data.map((post) => {
+                      const date = new Date(post.time);
+                      return {
+                          ...post,
+                          date: format(date, 'yyyy-MM-dd'),
+                        }
+                    })
+
+                const reduced = mappedData.reduce((acc, currentItem) => {
+                    const { date, ...item } = currentItem;
+                    {acc[date] ? acc[date].push({...item}) : acc[date] = [item] }
+                    return acc;
+                }, {});
+                setItems(reduced);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getData(getSavedToken);
+      }, []);
 
 
-    // useEffect(() => {
-    //     (async () => {
-    //         const response = await fetch("http://10.0.2.2:3001/tasks");
-    //         const data = await response.json();
-
-    //         const mappedData = data.map((post) => {
-    //             const date = new Date(post.Time);
-
-    //             return {
-    //                 ...post,
-    //                 date: format(date, 'yyyy-MM-dd'),
-    //             }
-    //         })
-
-    //         const reduced = mappedData.reduce((acc, currentItem) => {
-    //             const { date, ...item } = currentItem;
-    //             {acc[date] ? acc[date].push({...item}) : acc[date] = [item] }
-    //             return acc;
-    //         }, {});
-
-    //          setItems(reduced);
-    //         // console.log(timeToString(items[0].Time));
-    //     })();
-    // }, []);
-
-    
 
     return (
         <View style={{flex: 1}}>
             <Agenda
-                // items={items}
-                // selected={'2021-3-20'}
-                // renderItem={renderItem}
+                items={items}
+                renderItem={renderItem}
             />
         </View>
     );
