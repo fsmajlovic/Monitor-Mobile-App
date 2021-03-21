bd135a19b864c92128423f38ce7ba597e93be51b
bhodzic1 <bhodzic1@etf.unsa.ba>
2021-03-21 23:04:25 +0100

    Ispravljena greška

--- contexts/DeviceContext.js
+++ contexts/DeviceContext.js
@@ -11,23 +11,6 @@ export const DeviceProvider = (props) => {
     const { getSavedToken } = useContext(AuthContext);
     const [devices, setDevices] = useState([]);
 
-    useEffect(() => {
-        async function getData(getSavedToken) {
-            let token = await getSavedToken();
-            fetch("https://si-2021.167.99.244.168.nip.io/api/device/AllDevices", {
-                method: 'GET',
-                headers: { "Authorization": "Bearer " + token },
-            }).then((response) => {
-                return response.json();
-            }).then((responseJson) => {
-                setDevices(responseJson.data);
-            }).catch((error) => {
-                console.error(error);
-            });
-        }
-        getData(getSavedToken);
-    }, []);
-
     const values = {
         devices,
         setDevices
