4d059c5d8d7c0918cb1d622f2727f099531cb16a
skahvedzic1 <skahvedzic1@etf.unsa.ba>
2021-03-22 23:16:32 +0100

    Popravljena dugmad 'Add task' i 'Schedule'

--- screens/TehnicianScreen/TehnicianScreen.js
+++ screens/TehnicianScreen/TehnicianScreen.js
@@ -2,11 +2,23 @@ import React from 'react'
 import { StyleSheet, View, Button } from 'react-native';
 
 
-export default function TehnicianScreen({navigation}) {
-    return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-           <Button title="Add task" onPress={() => navigation.push("AddTask")}></Button>
-          <Button title="Schedule" onPress={() => navigation.push("Schedule")}></Button>
-        </View>
-      );
+export default function TehnicianScreen({ navigation }) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+       <View style={{width: 150, height: 50 }}>
+       <Button title="Add task" onPress={() => navigation.push("AddTask")}></Button>
+       <View style={styles.space} />
+      <Button title="Schedule" onPress={() => navigation.push("Schedule")}></Button>
+      </View>
+    </View>
+  );
 }
+
+
+
+const styles = StyleSheet.create({
+  space: {
+    width: 20, 
+    height: 20
+  },
+})
