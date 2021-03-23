4f1a24843919fe313d629010236955001edc9d30
bhodzic1 <bhodzic1@etf.unsa.ba>
2021-03-23 18:46:59 +0100

    Dodao nove screenove, izmijenjen raspored stackNav

--- contexts/DeviceContext.js
+++ contexts/DeviceContext.js
@@ -21,7 +21,7 @@ export const DeviceProvider = (props) => {
         activeDevices,
         addActiveDevice,
         activeDevice,
-        setActiveDevice,
+        setActiveDevice, 
         devices,
         setDevices
     }
