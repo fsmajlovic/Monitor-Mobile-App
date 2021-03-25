0f06e26a5084cc5590d435bc406ef996d30e0f8c
hcorbo <hcorbo2@etf.unsa.ba>
2021-03-25 16:51:49 +0100

    Uredio Tehnician screen

--- screens/TehnicianScreen/TehnicianScreen.js
+++ screens/TehnicianScreen/TehnicianScreen.js
@@ -1,24 +1,82 @@
 import React from 'react'
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Button } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
-
-export default function TehnicianScreen({ navigation }) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-       <View style={{width: 150, height: 50 }}>
-       <Button title="Add task" onPress={() => navigation.push("AddTask")}></Button>
-       <View style={styles.space} />
-      <Button title="Schedule" onPress={() => navigation.push("Schedule")}></Button>
-      </View>
-    </View>
-  );
+export default function TehnicianScreen({navigation}) {
+  return(
+       <View style={styles.container}>
+         <View  style={styles.containerTitle}>
+              <Text style={styles.textTitle}>Hi tehnician</Text>
+         </View>
+         <View style={{flexDirection: 'row', marginTop: 30}}>
+         <TouchableOpacity 
+                      onPress={() => navigation.push("AddTask")}
+                      style={styles.touchableOpacity}
+                  >
+                      <View style={styles.containerIcon}>
+                          <MaterialIcons name="note-add" size={80} color="#E50914" />
+                          <Text style={styles.iconText}>
+                              Add Task
+                          </Text>
+                      </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity 
+                      onPress={() => navigation.push("Schedule")}
+                      style={styles.touchableOpacity}
+                  >
+                      <View style={styles.containerIcon}>
+                          <Fontisto name="date" size={80} color="#E50914" />
+                          <Text style={styles.iconText}>
+                              Schedule
+                          </Text>
+                      </View>
+                  </TouchableOpacity>
+         </View>
+       </View>
+  )
+    
 }
 
-
-
 const styles = StyleSheet.create({
-  space: {
-    width: 20, 
-    height: 20
-  },
+container: {
+  flex: 1,
+  backgroundColor: "#FFF",
+},
+containerTitle: {
+  justifyContent: 'center',
+   backgroundColor:"#E50914",
+   height:"10%",
+   margin: 10,
+   borderRadius:30,
+   paddingLeft:30,
+   marginTop: 30
+},
+textTitle: {
+  fontSize:24,
+  color:"#FFF",
+  fontWeight:"bold"
+},
+touchableOpacity: {
+  height:250,
+  elevation:15,
+  backgroundColor:"white",
+  marginLeft:30,
+  marginTop:30,
+  borderRadius:15,
+  marginBottom:10,
+  width:160
+},
+containerIcon: {
+  flexDirection:"column",
+  alignItems: 'center',
+  paddingTop: 70
+},
+iconText: {
+  justifyContent: 'center',
+  fontWeight:"bold",
+  color:"#333",
+  fontSize: 16,
+  marginTop: 30
+}
 })
