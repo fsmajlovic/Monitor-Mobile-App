60e67ba72826e98cd699f46d76812ac47c5883f1
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2021-04-03 15:36:01 +0200

    Automatski fetch liste datoteka po otvaranju FM-a.

--- screens/AccessControlScreen/screens/FileManager.js
+++ screens/AccessControlScreen/screens/FileManager.js
@@ -9,47 +9,46 @@ import { AuthContext } from '../../../contexts/authContext';
 var image_url = "https://static.thenounproject.com/png/59103-200.png";
 
 export default function App({ navigation }) {
-
-
+  
   var [files, setFiles] = useState([]);
   var { getSavedToken } = React.useContext(AuthContext);
-
-  async function getFiles(token) {
-
-    const response = await fetch(serverURL + "api/web/user/fileList", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        Accept: "text/html",
-        Authorization: "Bearer " + token,
-      },
-      body: JSON.stringify({
-        user: "osoba4@email.com"
-      }),
-    });
-    var jsonResponse = await response.json();
-
-    var jsonResponseArray = jsonResponse['children'];
-    var newDataSet = [];
-    for (let i = 0; i < jsonResponseArray.length; i++) {
-      let file = jsonResponseArray[i];
-      newDataSet.push({ name: file['name'], id: (i + 1).toString(), image_url: image_url });
+  useEffect(() => {
+    async function getFiles() {
+      let token = await getSavedToken();
+      console.log('Token je: ' + token);
+      const response = await fetch(serverURL + "api/web/user/fileList", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "text/html",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify({
+          user: "osoba4@email.com"
+        }),
+      });
+      var jsonResponse = await response.json();
+  
+      var jsonResponseArray = jsonResponse['children'];
+      var newDataSet = [];
+      for (let i = 0; i < jsonResponseArray.length; i++) {
+        let file = jsonResponseArray[i];
+        newDataSet.push({ name: file['name'], id: (i + 1).toString(), image_url: image_url });
+      }
+      setFiles(newDataSet);
     }
-    setFiles(newDataSet);
-  }
-
+    try{
+      getFiles();
+    }catch(e){
+      console.log(e);
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
       <View style={{ alignItems: 'center' }}>
         <Text style={styles.text}>Files</Text>
       </View>
-      <Button onPress={async () => {
-        let token = await getSavedToken();
-        console.log("token je " + token);
-        getFiles(token);
-      }}
-        title={'Load files from server'} />
       <ListViewVertical
         itemList={files}
       />
